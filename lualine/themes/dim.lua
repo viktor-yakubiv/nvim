aaed40ea9d3b983c7ea41adcfe30d8cbcc4f87ac
Viktor Yakubiv <viktor@yakubiv.com>
2023-01-04 12:20:08 +0200

    Lualine theme refactored ♻️

--- lualine/themes/dim.lua
+++ lualine/themes/dim.lua
@@ -1,34 +1,51 @@
-local colors = {
-  black   = 0,
-  maroon  = 1,
-  green   = 2,
-  olive   = 3,
-  navy    = 4,
-  purple  = 5,
-  teal    = 6,
-  silver  = 7,
-  gray    = 8,
-  red     = 9,
-  lime    = 10,
-  yellow  = 11,
-  blue    = 12,
-  fuchsia = 13,
-  aqua    = 14,
-  white   = 15,
+local C = {
+  Black         = 0,
+  DarkRed       = 1,
+  DarkGreen     = 2,
+  Brown         = 3,
+  DarkBlue      = 4,
+  DarkMagenta   = 5,
+  DarkCyan      = 6,
+  LightGrey     = 7,
+  Silver        = 7,
+  Grey          = 8,
+  DarkGrey      = 8,
+  BrightRed     = 9,
+  Red           = 9,
+  BrightGreen   = 10,
+  Green         = 10,
+  Yellow        = 11,
+  Blue          = 12,
+  BrightBlue    = 12,
+  Magenta       = 13,
+  BrightMagenta = 13,
+  Cyan          = 14,
+  BrightCyan    = 14,
+  White         = 15,
 }
+
+if vim.o.background == 'light' then
+  local Background = C.LightGrey
+  local Major = C.Black
+  local Minor = C.DarkGrey
+else 
+  local Background = C.Black
+  local Major = C.White
+  local Minor = C.LightGrey
+end
 
 return {
   normal = {
-    a = { fg = colors.white, bg = colors.navy, gui = 'bold' },
-    b = { fg = colors.white, bg = colors.gray },
-    c = { fg = colors.silver, bg = colors.black },
+    a = { fg = C.White, bg = C.DarkBlue, gui = 'bold' },
+    b = { fg = Major, bg = Background },
+    c = { fg = Minor, bg = Background },
   },
-  insert = { a = { fg = colors.white, bg = colors.green, gui = 'bold' } },
-  visual = { a = { fg = colors.white, bg = colors.purple, gui = 'bold' } },
-  replace = { a = { fg = colors.white, bg = colors.maroon, gui = 'bold' } },
+  insert = { a = { fg = C.White, bg = C.DarkGreen, gui = 'bold' } },
+  visual = { a = { fg = C.White, bg = C.DarkMagenta, gui = 'bold' } },
+  replace = { a = { fg = C.White, bg = C.DarkRed, gui = 'bold' } },
   inactive = {
-    a = { fg = colors.silver, bg = colors.gray, gui = 'bold' },
-    b = { fg = colors.gray, bg = colors.black },
-    c = { fg = colors.silver, bg = colors.black },
+    a = { fg = C.LightGrey, bg = C.DarkGrey, gui = 'bold' },
+    b = { fg = Minor, bg = Background },
+    c = { fg = Major, bg = Background },
   },
 }
