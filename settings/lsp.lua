27c397c9fb073f5b7133b4fe5fb973d1114a56a9
Viktor Yakubiv <viktor@yakubiv.com>
2023-02-04 13:48:24 +0200

    Missing LSP config added 🐛

---
+++ settings/lsp.lua
@@ -0,0 +1,17 @@
+local M = {}
+
+M.servers = {
+	html = {
+		autoCreateQuotes = false,
+		-- completion.attributeDefaultValue = "doublequotes",
+		format = {
+			wrapLineLength = 80,
+		},
+	},
+
+	emmet_ls = {
+		filetypes = { 'html', 'css', 'sass', 'scss' },
+	},
+}
+
+return M
