91b5e0341c535d229359dce587126aa9e411c64e
Viktor Yakubiv <viktor@yakubiv.com>
2024-01-06 16:52:12 +0200

    Snippets for markdown

---
+++ luasnippets/markdown.lua
@@ -0,0 +1,16 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local sn = ls.snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+
+local function expand(s)
+	local tw = vim.api.nvim_buf_get_option(0, 'textwidth')
+	local n = math.floor(tw / s:len())
+	return t(string.rep(s, n))
+end
+
+return {
+	s({ trig = '^%-+', trigEngine = 'pattern' }, expand('-')),
+	s({ trig = '^%=+', trigEngine = 'pattern' }, expand('=')),
+}
