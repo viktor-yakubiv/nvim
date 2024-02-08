73a3f2e4ef0394af9b32c47b5fb786f298f839e1
Viktor Yakubiv <viktor@yakubiv.com>
2024-02-08 11:03:10 +0200

    Netrw disabled in favour of nvim-tree 🔧

--- plugins/tree.lua
+++ plugins/tree.lua
@@ -1,7 +1,12 @@
 local tree = require 'nvim-tree'
 local api = require 'nvim-tree.api'
 
-tree.setup()
+tree.setup {
+	-- Netrw is a standard file explorer in Neovim.
+	-- Despite it has other features, I have not used them,
+	-- so disabling this according to the recommendation in the docs.
+	disable_netrw = true,
+}
 
 -- Keymaps
 vim.keymap.set('n', '<leader>tf', api.tree.focus)
