2816c60c3a50bbbe1d6a401546a55b02089ebd60
Viktor Yakubiv <viktor@yakubiv.com>
2024-02-18 13:28:47 +0200

    Syntax plugins migrated to own category ♻️

--- lua/yakubiv/init.lua
+++ lua/yakubiv/init.lua
@@ -1,11 +1,6 @@
 local plugins = require 'yakubiv.plugins'
 plugins.autoload()
 
-plugins.use {
-	--- Syntax
-	{ 'dannywillems/vim-icalendar', name = "icalendar", ft = "icalendar" },
-}
-
 require 'yakubiv.options'
 require 'yakubiv.appearance'
 require 'yakubiv.keymaps'
---
+++ lua/yakubiv/plugins/syntax.lua
@@ -0,0 +1,4 @@
+return {
+	-- I edit iCalendar files sometimes
+	{ 'dannywillems/vim-icalendar', name = "icalendar", ft = "icalendar" },
+}
