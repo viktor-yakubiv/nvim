caf716637d895366943f0cd7931512afacf7c12a
Viktor Yakubiv <viktor@yakubiv.com>
2024-07-11 11:42:59 +0300

    Trying Noice for messages

--- lua/yakubiv/plugins/appearance.lua
+++ lua/yakubiv/plugins/appearance.lua
@@ -22,4 +22,53 @@ return {
 
 	-- File icons that many plugins use, e.g. nvim-tree.lua, neo-tree, telescope
 	{ 'nvim-tree/nvim-web-devicons', opts = {}, lazy = true },
+
+	{
+		"folke/noice.nvim",
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+			"rcarriga/nvim-notify",
+		},
+		event = "VeryLazy",
+		opts = {
+			lsp = {
+				-- override markdown rendering
+				-- so that **cmp** and other plugins use **Treesitter**
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+					["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+				},
+			},
+			cmdline = {
+				view = "cmdline",
+			},
+			messages = {
+				view = "mini",
+			},
+			presets = {
+				bottom_search = true,
+				command_palette = false,
+				long_message_to_split = true,
+				inc_rename = false, -- enables an input dialog for inc-rename.nvim
+				lsp_doc_border = true, -- add a border to hover docs and signature help
+			},
+		},
+
+		init = function (plugin, opts)
+			require("noice").setup(opts)
+
+			require("notify").setup {
+				-- This colour is used to detect background lightness.
+				-- In terminal, I use transparent background
+				-- relying on the terminal's UI.
+				-- Ideally, this could track the theme's background from Catppuccin
+				-- based on the current vim.opt.background
+				-- but for the sake of simplicity I don't care —
+				-- I need to remove annoying message
+				background_colour = "#000000"
+			}
+		end,
+
+	},
 }
