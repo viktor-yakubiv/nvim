4c8e743e776dc4770bc6aeb2fbae988a372f223f
Viktor Yakubiv <viktor@yakubiv.com>
2025-03-07 21:58:40 +0200

    Zotero for Telescope installed ➕

--- lua/yakubiv/plugins/latex.lua
+++ lua/yakubiv/plugins/latex.lua
@@ -8,4 +8,16 @@ return {
 			telecope.load_extension "bibtex"
 		end,
 	},
+	{
+		"jmbuhr/telescope-zotero.nvim",
+		dependencies = {
+			"nvim-telescope/telescope.nvim",
+			"kkharji/sqlite.lua",
+		},
+		config = function()
+			local telescope = require "telescope"
+			telescope.load_extension "zotero"
+		end,
+		ft = { "tex", "bib" },
+	},
 }
