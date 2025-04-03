b5ffa609f43a050d06e92da748fb04c9784b8f5c
Viktor Yakubiv <viktor@yakubiv.com>
2025-04-03 22:14:06 +0300

    Play with local LLMs

---
+++ lua/yakubiv/plugins/llm.lua
@@ -0,0 +1,11 @@
+return {
+	{
+		"olimorris/codecompanion.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		event = "VeryLazy",
+		config = true,
+	},
+}
