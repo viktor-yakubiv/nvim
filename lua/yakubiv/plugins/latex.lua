465807d6c1df5cff523992033024a2b7b3408d7e
Viktor Yakubiv <viktor@yakubiv.com>
2025-06-19 16:30:05 +0300

    Prettify LaTeX plugins config

--- lua/yakubiv/plugins/latex.lua
+++ lua/yakubiv/plugins/latex.lua
@@ -1,23 +1,26 @@
 return {
 	{
 		"nvim-telescope/telescope-bibtex.nvim",
-		dependencies = { "nvim-telescope/telescope.nvim" },
+		dependencies = {
+			"nvim-telescope/telescope.nvim",
+		},
 		ft = { "tex", "bib" },
 		config = function()
-			local telecope = require "telescope"
-			telecope.load_extension "bibtex"
+			local telescope = require "telescope"
+			telescope.load_extension "bibtex"
 		end,
 	},
+
 	{
 		"jmbuhr/telescope-zotero.nvim",
 		dependencies = {
 			"nvim-telescope/telescope.nvim",
 			"kkharji/sqlite.lua",
 		},
+		ft = { "tex", "bib" },
 		config = function()
 			local telescope = require "telescope"
 			telescope.load_extension "zotero"
 		end,
-		ft = { "tex", "bib" },
 	},
 }
